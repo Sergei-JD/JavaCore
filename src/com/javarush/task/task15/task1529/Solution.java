835c49787b35a29c729887cf5edae9adf6423756
Sergei-JD <sergshlyazhko@gmail.com>
2021-08-24 19:52:12 +0300

    javaRush 15 level add 19 - 25

---
+++ src/com/javarush/task/task15/task1529/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task15.task1529;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Осваивание статического блока
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static {
+        reset();
+    }
+
+    public static CanFly result;
+
+    public static void reset() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String str = reader.readLine();
+
+            if("plane".equals(str)) {
+                str = reader.readLine();
+                result = new Plane(Integer.parseInt(str));
+            }else if("helicopter".equals(str)) {
+                result = new Helicopter();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
