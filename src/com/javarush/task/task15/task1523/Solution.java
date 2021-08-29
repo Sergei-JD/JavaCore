5d651ec6eb884e67be13c74f78007dc4412cafa4
Sergei-JD <sergshlyazhko@gmail.com>
2021-08-29 20:15:32 +0300

    javaRush 15 level add 26 - 28

---
+++ src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    private Solution() {
+    }
+
+    protected Solution(int i) {
+    }
+
+    Solution(long l) {
+    }
+
+    public Solution(double d) {
+    }
+}
+
